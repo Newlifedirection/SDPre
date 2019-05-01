e021963f87740aadf151e4404f5b9a9d73fc8930
Newlifedirection <craig.dreiling@gmail.com>
2019-05-01 08:27:38 -0600

    changes to Ovj2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Pizza";
+    food2 = "Mexican food";
+    food3 = "Indian food";
+
+    System.out.println("My favorite foods are:");
+    System.out.println(food1);
+    System.out.println(food2);
+    System.out.println(food3);
+  }
+}
