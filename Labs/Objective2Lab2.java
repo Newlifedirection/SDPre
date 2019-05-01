25f674dcce4a0e2cfba8a040d2e55d3af4651365
Newlifedirection <craig.dreiling@gmail.com>
2019-05-01 08:20:35 -0600

    finish Obj2Lab1

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Craig Dreiling";
+    int age = 40;
+    String state = "Colorado";
+
+    System.out.print("Name: " + name + "\tAge: " + age + "\tState: " + state );
+  }
+}
