88c444d90319e36cc21394db477cdb76d75f086c
Newlifedirection <craig.dreiling@gmail.com>
2019-05-02 08:12:59 -0600

    finish Obj4Lab1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -3,13 +3,13 @@ public class Objective4Lab1 {
     int num1 = 5;
     int num2 = 15;
 
-    System.out.println("which is superior, num1 or num2?");
+    System.out.println("which is greater, num1 or num2?");
 
     if(num1 > num2) {
-      System.out.println("num1 is superior");
+      System.out.println("num1 is greater");
     }
     else if(num2 > num1){
-      System.out.println("num2 is superior");
+      System.out.println("num2 is greater");
     }
   }
 }
