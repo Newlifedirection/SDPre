b511aa28b7e80289a4a7c9b85d3aab7c2d52aa7e
Newlifedirection <craig.dreiling@gmail.com>
2019-05-02 10:45:55 -0600

    finish Obj4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Objective4Lab3 {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int userNum = 0;
+
+    System.out.println("Please enter a number: ");
+    userNum = scanner.nextInt();
+    System.out.println("The number is positive");
+      if ( userNum > 0 );
+
+    scanner.close();
+  }
+}
