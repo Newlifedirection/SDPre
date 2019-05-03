de1dd4f14474c60e97dee69d27d012a33f615feb
Newlifedirection <craig.dreiling@gmail.com>
2019-05-03 15:07:03 -0600

    finally got Obj6Lab3

--- Labs/Objective6Lab3.java
+++ Labs/Objective6Lab3.java
@@ -1,26 +1,17 @@
-//import java.util.Scanner;
 public class Objective6Lab3 {
   public static void main(String[] args) {
     int counter = 1;
 
-    while(counter <= 20);
+    while(counter <= 20){
 
     if(counter%2 == 0)
-      System.out.println(counter+ "is even");
+      System.out.println(counter+ " is even");
     if(counter%2 !=0)
-      System.out.println(counter+ "is odd");
-    //else
-  //if (counter%2 == 0)
-      //System.out.println("2 is even");
-  //else
-      //System.out.println("1 is odd");
-
-    //do {
-
-
+    
+      System.out.println(counter+ " is odd");
       counter++;
-    //}
-    //while(counter <= 19);
 
+
+    }
   }
 }
